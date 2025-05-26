eb7a3b60985cb8f0d715ef0bdbd9cdeafb896c74
jonatas.oliveira <jonatas.oliveira@fornecedores.goias.gov.br>
2025-05-26 19:27:30 -0300

    Swagger

--- src/main/java/com/senai/jonatas/aluguelveiculoapi/AluguelVeiculoApiApplication.java
+++ src/main/java/com/senai/jonatas/aluguelveiculoapi/AluguelVeiculoApiApplication.java
@@ -3,6 +3,13 @@ package com.senai.jonatas.aluguelveiculoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "${info.build.name}",
+                version = "${info.build.version}",
+                description = "${info.app.description}",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Time Arquitetura e APIs",
+                        email = "jonatas.edo@gmail.com")))
 @SpringBootApplication
 public class AluguelVeiculoApiApplication {
 
